4bc1b37d16b7324869e030c42d678ea2e9fcc1fc
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-04-18 17:44:29 +0530

    Variables in js

---
+++ scripts/3-Variables.js
@@ -0,0 +1,17 @@
+
+// Variables
+
+/* 
+Variables are containers that store values. You start by declaring a variable with the let keyword, followed by the name you give to the variable:
+*/
+
+// let myVariable;
+
+// myVariable = "Bob";
+
+// After assigning a value to a variable, you can change it later in the code:
+
+// let myVariable = "Bob";
+// myVariable = "Steve";
+
+// console.log(myVariable);
