29cdb467b399d878012482435cc02885b64b4c25
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-04-18 17:44:57 +0530

    Functions in js

---
+++ scripts/5-Functions.js
@@ -0,0 +1,25 @@
+// Functions
+
+/*
+Functions are a way of packaging functionality that you wish to reuse.
+ It's possible to define a body of code as a function that executes when you call the function name in your code. 
+ This is a good alternative to repeatedly writing the same code. You have already seen some uses of functions. 
+*/
+
+// document.querySelector and alert, are built into the browser.
+
+let myVariable = document.querySelector("h1");
+
+alert("hello!");
+
+// User-defined functions
+
+function multiply(num1, num2) {
+  let result = num1 * num2;
+  return result;
+}
+
+
+console.log(multiply(4, 7));
+// multiply(20, 20);
+// multiply(0.5, 3);
