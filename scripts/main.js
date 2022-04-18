b88ff9fb8c4437e9f8b5effd2fb6b0729f1eda4f
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-04-18 17:45:39 +0530

    Testing functions

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,13 @@ const myHeading = document.querySelector('h1')
 myHeading.textContent = "Hello World"  //  The code set the value of the myHeading variable's textContent property (which represents the content of the heading) to Hello world!.
 
 
+
+
+function multiply(num1, num2) {
+  let result = num1 * num2;
+  return result;
+}
+
+console.log(multiply(4, 7));
+// multiply(20, 20);
+// multiply(0.5, 3);
