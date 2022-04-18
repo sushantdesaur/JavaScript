362faa89474ba7f7de2f83fb4bd2932e3e8d7fdc
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-04-18 17:44:17 +0530

    QuerySelector

---
+++ scripts/2-Query-selector.js
@@ -0,0 +1,23 @@
+// Document.querySelector()
+
+/* 
+The Document method querySelector() returns the first Element within the document that matches the specified selector, or group of selectors.
+If no matches are found, null is returned.
+*/
+
+const myHeading = document.querySelector("h1");
+
+//  The code set the value of the myHeading variable's textContent property (which represents the content of the heading) to Hello world!.
+myHeading.textContent = "Hello World"; //  The code set the value of the myHeading variable's textContent property (which represents the content of the heading) to Hello world!.
+
+// Variables
+
+/* 
+Variables are containers that store values. You start by declaring a variable with the let keyword, followed by the name you give to the variable:
+*/
+
+let myVariable;
+
+myVariable = "Bob";
+
+
